bb92a02cc48d59cbca3fe1e7372ea206c867456b
josewweee <jose>
2021-03-20 19:45:51 -0500

    v3 carta

--- main.js
+++ main.js
@@ -1,1 +1,3 @@
-window.location.replace("https://drive.google.com/file/d/1brqLlc1dGuI2BNLuP2ccZSoaGwikDcPg/view?usp=sharing");
+window.location.replace(
+  'https://drive.google.com/file/d/1cNO6xhu-XJme335gny9Z2f9oYu5s3of7/view?usp=sharing'
+);
---
+++ public/main.js
@@ -0,0 +1,3 @@
+window.location.replace(
+  'https://drive.google.com/file/d/1hiGWMVCN3bzRt2L0Ay7FVOHH-Eq3oefM/view?usp=sharing'
+);
